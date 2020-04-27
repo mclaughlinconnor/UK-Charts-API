bd399fe2adffaf4fc7562f3bbeaa4e314ce56576
Connor McLaughlin <connor1211.mcl@gmail.com>
2020-04-27 10:10:16 +0100

    Switch to python logging

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import logging
 import multiprocessing
 import signal
 
@@ -22,16 +23,16 @@ def timeout_handler(signum: int, frame) -> None:  # type: ignore
 
 def worker(chart_item: chart.ChartData) -> None:
     signal.alarm(1)
-    print(f"{chart_item.chart_song.title} started.")
+    logging.info(f"{chart_item.chart_song.title} started.")
     try:
         d = chart_item.chart_song.to_deezer()
         dl = download.Download(d.track_id, f"{d.generate_filepath('/home/connor/UK Charts API Refactor/output')}", d)
         dl.download()
     except ValueError:
-        print(f"{chart_item.chart_song.title} failed.")
+        logging.info(f"{chart_item.chart_song.title} failed.")
         # Logic for failed to_deezer calls here.
     except exception.TimeoutException:
-        print(f"{chart_item.chart_song.title} timed out.")
+        logging.info(f"{chart_item.chart_song.title} timed out.")
     finally:
         signal.alarm(0)
         return
@@ -39,7 +40,7 @@ def worker(chart_item: chart.ChartData) -> None:
 
 if __name__ == "__main__":
     signal.signal(signal.SIGALRM, timeout_handler)
-    # multiprocessing.set_start_method("spawn")
+    logging.basicConfig(level=logging.INFO)
     for year in range(1954, 2020):
         dates = utils.generate_dates(year)
         for date in dates:
