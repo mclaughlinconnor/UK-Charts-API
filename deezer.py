c7cd8645aa4b47c203ee22aeff0d8d08afe8cf89
Connor McLaughlin <connor1211.mcl@gmail.com>
2020-04-28 20:49:04 +0100

    Move Endpoints to own file

--- deezer.py
+++ deezer.py
@@ -2,33 +2,9 @@ from typing import Dict, Union, Optional
 
 import requests
 
+from endpoints import Endpoints
 from models import deezer
 from utils import requests_retry_session, replace_illegals
-
-
-class Endpoints:
-    @staticmethod
-    def track(track_id: str) -> str:
-        return f"https://api.deezer.com/track/{track_id}"
-
-    @staticmethod
-    def contributor(contributor_id: str) -> str:
-        return f"https://api.deezer.com/artist/{contributor_id}"
-
-    @staticmethod
-    def album(album_id: str) -> str:
-        return f"https://api.deezer.com/album/{album_id}"
-
-    @staticmethod
-    def search(parameters: Dict[str, Union[str, int]]) -> str:
-        end_point = "https://api.deezer.com/search?q="
-        for parameter, value in parameters.items():
-            end_point += f"{parameter}:'{value}' "
-        return end_point
-
-    @staticmethod
-    def get_user_data() -> str:
-        return "deezer.getUserData"
 
 
 class Deezer:
