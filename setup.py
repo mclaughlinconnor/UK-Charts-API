474e070d54d2092520c40ecb074d85332e51fd5e
Connor McLaughlin <connor1211.mcl@gmail.com>
2020-05-07 21:27:06 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="uk_charts_api",
-    version="1.0",
+    version="1.0.1",
     author="Connor McLaughlin",
     description="A small example package",
     long_description=long_description,
